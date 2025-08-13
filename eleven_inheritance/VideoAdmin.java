181adc9df088141482318b7d1b0732cb44e6c6ea
ravi2krishna <ravi2krishna@gmail.com>
2025-08-13 11:31:45 +0530

    After Inheritance

--- eleven_inheritance/VideoAdmin.java
+++ eleven_inheritance/VideoAdmin.java
@@ -1,29 +1,29 @@
 package eleven_inheritance;
 
-public class VideoAdmin {
+public class VideoAdmin extends Guest {
 
     // watch video - 500 line of code
-    public static void watchVideo() {
-        System.out.println("W");
-        System.out.println("A");
-        System.out.println("T");
-        System.out.println("C");
-        System.out.println("H");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println("V");
-        System.out.println("I");
-        System.out.println("D");
-        System.out.println("E");
-        System.out.println("O");
-    }
+    // public static void watchVideo() {
+    //     System.out.println("W");
+    //     System.out.println("A");
+    //     System.out.println("T");
+    //     System.out.println("C");
+    //     System.out.println("H");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println(".");
+    //     System.out.println("V");
+    //     System.out.println("I");
+    //     System.out.println("D");
+    //     System.out.println("E");
+    //     System.out.println("O");
+    // }
 
     // Add video - 1000 line of code
     public static void addVideo() {
