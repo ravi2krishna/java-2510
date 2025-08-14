4561f08ee50552075097bceade7189418ef5f4ad
ravi2krishna <ravi2krishna@gmail.com>
2025-08-14 11:13:35 +0530

    Hybrid Inheritance

---
+++ eleven_inheritance/NewGuest.java
@@ -0,0 +1,10 @@
+package eleven_inheritance;
+
+public class NewGuest implements Watchable {
+
+    
+    public void watchVideo() {
+        System.out.println("Watch Video");
+    }
+    
+}
---
+++ eleven_inheritance/NewVideoAdmin.java
@@ -0,0 +1,10 @@
+package eleven_inheritance;
+
+public class NewVideoAdmin extends NewGuest implements Addable {
+
+    
+    public void addVideo() {
+        System.out.println("Add Video");
+    }
+
+}
