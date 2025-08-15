9ecfc288abed199c9bb79f4d03a5b673fbe0d875
ravi2krishna <ravi2krishna@gmail.com>
2025-08-15 11:55:43 +0530

    Top Level Classes

---
+++ pack1/OuterClass.java
@@ -0,0 +1,20 @@
+package pack1;
+
+class OuterClass {
+
+    public class PublicInner {
+
+    }
+
+    protected class ProtectedInner {
+
+    }
+
+    class DefaultInner {
+
+    }
+
+    private class PrivateInner {
+
+    }
+}
