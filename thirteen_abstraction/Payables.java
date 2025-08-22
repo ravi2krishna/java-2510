707ec36bed0600eabb184cf37ad8c147a2680fe5
ravi2krishna <ravi2krishna@gmail.com>
2025-08-22 11:59:53 +0530

    LMS With Abstraction - P1

---
+++ thirteen_abstraction/Payables.java
@@ -0,0 +1,8 @@
+package thirteen_abstraction;
+
+public interface Payables {
+
+    // contract --> must have 
+    public void calculatePayment();
+    
+}
---
+++ thirteen_abstraction/Personable.java
@@ -0,0 +1,10 @@
+package thirteen_abstraction;
+
+public interface Personable {
+
+    // contract --> must have 
+    public void setPersonDetails();
+    public void displayPersonDetails();
+    public void achievementStatus();
+
+}
